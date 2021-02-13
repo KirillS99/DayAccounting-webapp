731dba1db580e082232f39316afe16f464a8a403
SeltanRush <akuznetsov.sel@gmail.com>
2021-02-13 19:55:08 +0700

    Added error interceptor

--- src/services/api/BaseHttpService.ts
+++ src/services/api/BaseHttpService.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import { AccessTokenManager } from '../../shared/managers/AccessTokenManager';
 
 export interface IHttpMethodRequestSettings {
@@ -10,11 +10,31 @@ const makeAuthorizationHeaders = (accessTokenManager: AccessTokenManager) => ({
   Authorization: `Bearer ${accessTokenManager.getAccessToken()}`,
 });
 
+class ResponseErrorInterceptor {
+  constructor(private accessTokenManager: AccessTokenManager) {}
+
+  public intercept = (error: AxiosError): AxiosError => {
+    if (error.code === '401') {
+      this.accessTokenManager.removeAccessToken();
+    }
+
+    return error;
+  };
+}
+
 export class BaseHttpService {
   private client: AxiosInstance;
 
   constructor(private accessTokenManager: AccessTokenManager) {
     this.client = axios.create();
+    this.client.defaults.responseType = 'json';
+    this.client.defaults.timeout = 30000;
+    this.client.defaults.validateStatus = (status) =>
+      (status >= 200 && status < 300) || status === 302;
+    this.client.interceptors.response.use(
+      undefined,
+      new ResponseErrorInterceptor(this.accessTokenManager).intercept
+    );
   }
 
   protected get = <Response>({ url, data }: IHttpMethodRequestSettings) => {
