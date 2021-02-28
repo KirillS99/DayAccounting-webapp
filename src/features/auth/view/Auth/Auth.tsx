4db3e726d4b9aca19c6d61997f750205f7c1fb49
Kirill Buryak <buryak.1998@gmail.com>
2021-02-28 12:17:04 +0300

    added link to redirect

--- src/features/auth/view/Auth/Auth.tsx
+++ src/features/auth/view/Auth/Auth.tsx
@@ -34,7 +34,11 @@ const Auth: React.FC<IProps> = ({ authenticatingByGoogle, loginByGoogle }) => {
   return (
     <div className={styles.root}>
       <div className={styles.content}>
-        <a href="https://day-accounting.herokuapp.com/api/google">Google</a>
+        <a
+          href={`http://localhost:3000/api/google?authRedirect=${location.origin}`}
+        >
+          Google
+        </a>
       </div>
     </div>
   );
