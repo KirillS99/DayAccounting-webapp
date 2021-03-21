12289bee62371578c8f623a25aaac03595a60745
Kirill Buryak <buryak.1998@gmail.com>
2021-03-21 14:14:01 +0300

    testing baseurl

--- src/services/api/users/UsersService.ts
+++ src/services/api/users/UsersService.ts
@@ -1,11 +1,15 @@
 import { BaseHttpService } from '../BaseHttpService';
 
 import { IUser } from 'shared/models/User';
+import { AccessTokenManager } from 'shared/managers/AccessTokenManager';
 
 export class UsersService extends BaseHttpService {
+  constructor(accessTokenManager: AccessTokenManager, baseUrl: string) {
+    super(accessTokenManager, baseUrl);
+  }
   public async getCurrentUser(): Promise<IUser> {
     const user = await this.get<IUser>({
-      url: '/api/user/current-user',
+      url: `${this.baseUrl}/api/user/current-user`,
     });
 
     return user.data;
